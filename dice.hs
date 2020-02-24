abd196e2d207fe9c46b3681ab08f1b6e07d97942
Kipav <55412120+Kipav@users.noreply.github.com>
2020-02-24 16:15:18 +0100

    Update dice.hs

--- dice.hs
+++ dice.hs
@@ -10,4 +10,20 @@ twoDice :: IO ()
 twoDice = do
     number <- randomRIO (1,6) :: IO Int
     number2 <- randomRIO (1,6) :: IO Int
-    putStrLn (show number ++ " and " ++ show number2)
+    if number == number2 then 
+        putStrLn (show number ++ " and " ++ show number2 ++ " Double!")
+        else putStrLn (show number ++ " and " ++ show number2)
+
+startGame :: IO ()
+startGame = do 
+    number1 <- randomRIO (1,6) :: IO Int
+    putStrLn "Player 1, press any button to roll the dice" 
+    _ <- getLine
+    putStrLn (show number1)
+    number2 <- randomRIO (1,6) :: IO Int
+    putStrLn "Player 2, press any button to roll the dice"
+    _ <- getLine
+    putStrLn (show number2)
+    if number1 > number2 then putStrLn ("Player 1 starts!")
+    else if number2 > number1 then putStrLn ("Player 2 starts!")
+    else startGame
