7c54698a25122b0a0106d51ac4a1ffecf2ae5433
Kipav <55412120+Kipav@users.noreply.github.com>
2020-03-02 12:03:38 +0100

    Update dice.hs

--- dice.hs
+++ dice.hs
@@ -17,11 +17,11 @@ twoDice = do
 startGame :: IO ()
 startGame = do 
     number1 <- randomRIO (1,6) :: IO Int
-    putStrLn "Player 1, press any button to roll the dice" 
+    putStrLn "Player 1, enter any button to roll the dice" 
     _ <- getLine
     putStrLn (show number1)
     number2 <- randomRIO (1,6) :: IO Int
-    putStrLn "Player 2, press any button to roll the dice"
+    putStrLn "Player 2, enter any button to roll the dice"
     _ <- getLine
     putStrLn (show number2)
     if number1 > number2 then putStrLn ("Player 1 starts!")
@@ -33,3 +33,18 @@ calculateMoves = do
     number2 <- randomRIO (1,6) :: IO Int
     if number == number2 then return [number, number, number2, number2]
         else return [number,number2]
+
+chooseDice = do
+    dices <- calculateMoves
+    putStrLn "Choose a dice" 
+    print dices
+    dice <- readLn
+    if number == number2 then 
+        putStrLn (show number ++ ", " ++ show number ++ ", " ++ show number2 ++ ", " ++ show number2)
+        else putStrLn (show number ++ ", " ++ show number2)
+    do
+        dice <- readLn
+        if (dice :: Int) == number then print number
+        else if (dice :: Int) == number2 then print number2
+        else putStrLn "Choose a dice that exists!"
+
